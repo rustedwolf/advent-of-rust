b1517c1be161bd5a11cb6b1a267fd4553e614d78
Ryszard <rustedwolf@gmail.com>
2016-05-16 00:10:04 +0200

    Day 9: redone the script

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -3,97 +3,54 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-#[derive(Clone)]
 struct Distance {
     point_a: String,
     point_b: String,
     value: u32
 }
-
 type Distances = Vec<Distance>;
 
-trait PointDistances {
-    fn get_shortest_distance(&self, starting_point: &str) -> Distance;
-    fn pic_a_spot(&mut self) -> String;
-    fn get_other_shortest_distance(&self, starting_point: &str, ignored_point: &str) -> u32;
-    fn sort_distances(&mut self);
-    fn remove_distances(&mut self, old_point: &str);
+struct Route {
+    places: Places,
+    distance: u32
+}
+type Routes = Vec<Route>;
+
+type Places = Vec<String>;
+
+trait RouteCalculations {
+    fn get_distances(&mut self) -> (u32, u32);
 }
 
-impl PointDistances for Distances {    
-    fn get_shortest_distance(&self, starting_point: &str) -> Distance {
-        let mut available_distances = self.clone();
-        available_distances.retain(|dist|{
-           dist.point_a == starting_point || dist.point_b == starting_point
-        });
-        available_distances.sort_distances();
-        available_distances.reverse();
-        available_distances.pop().unwrap()
-    }
-
-    fn pic_a_spot(&mut self) -> String {
-        self.sort_distances();
-
-        let point_a = &self[0].point_a;
-        let point_b = &self[0].point_b;
-        let distance_a_value = self.get_other_shortest_distance(point_a, point_b);
-        let distance_b_value = self.get_other_shortest_distance(point_b, point_a);
-
-        if distance_a_value > distance_b_value { 
-            point_a.to_string() 
-        } else { 
-            point_b.to_string() 
-        }
-    }
-
-    fn get_other_shortest_distance(&self, starting_point: &str, ignored_point: &str) -> u32 {
-        let mut other_distances = self.clone();
-        other_distances.retain(|dist|{
-            dist.point_a != ignored_point &&
-            dist.point_b != ignored_point &&
-            (dist.point_a == starting_point || dist.point_b == starting_point)
-        });
-        other_distances.sort_distances();
-        other_distances[0].value
-    }
-
-    fn sort_distances(&mut self) {
-        self.sort_by(|a, b| a.value.cmp(&b.value));
-    }
-
-    fn remove_distances(&mut self, old_point: &str) {
-        self.retain(|dist| {
-            dist.point_a != old_point && dist.point_b != old_point
-        });
+impl RouteCalculations for Routes {
+    fn get_distances(&mut self) -> (u32, u32) {
+        self.sort_by(|a, b| a.distance.cmp(&b.distance));
+        (self.first().unwrap().distance, self.last().unwrap().distance)
     }
 }
 
 fn main() {
     println!("--- Day 9: All in a Single Night ---");
 
-    let mut full_distance = 0;
-    let mut distances = get_distances();
-    let mut current_point = distances.pic_a_spot();
-    let mut distance = distances.get_shortest_distance(&current_point);
+    let distances = get_distances();
+    let places = get_places(&distances);
+    let mut routes: Routes = Vec::new();
 
-    'running: loop {
-        full_distance += distance.value;
-        let old_point = current_point.clone();
-        current_point = if current_point == distance.point_a {
-            distance.point_b
-        } else {
-            distance.point_a
-        }.clone();
-
-        distances.remove_distances(&old_point);
-        if !distances.is_empty() {
-            distance = distances.get_shortest_distance(&current_point);
-        } else {
-            break 'running;
-        }
+    for place in &places {
+        let route = Route {
+            places: vec![place.clone()],
+            distance: 0
+        };
+        routes.push(route);
     }
 
-    println!("The distance of the shortest route is: {}", full_distance);
+    for _ in 1..places.len() {
+        routes = generate_routes(&routes, &places, &distances);
+    }
+
+    let (shortest_route, longest_route) = routes.get_distances();
+
+    println!("The distance of the shortest route is {} and the longest is {}", shortest_route, longest_route);
 }
 
 fn get_distances() -> Distances {
@@ -119,4 +76,51 @@ fn get_distances() -> Distances {
     }
 
     distances
+}
+
+fn get_places(distances: &Distances) -> Places {
+    let mut places: Places = Vec::new();
+
+    for distance in distances {
+        if !places.contains(&distance.point_a) {
+            places.push(distance.point_a.clone())
+        }
+        if !places.contains(&distance.point_b) {
+            places.push(distance.point_b.clone())
+        }
+    }
+
+    places
+}
+
+fn generate_routes(routes: &Routes, places: &Places, distances: &Distances) -> Routes {
+    let mut updated_routes: Routes = Vec::new();
+    for route in routes {
+        for place in places {
+            if !route.places.contains(&place) {
+                let mut route_places = route.places.clone();
+                route_places.push(place.clone());
+                let new_route = Route {
+                    places: route_places,
+                    distance: route.distance + get_distance(route.places.last().unwrap(), &place, &distances),                    
+                };
+                updated_routes.push(new_route);
+            }
+        }
+    }
+
+    updated_routes
+}
+
+fn get_distance(place_a : &str, place_b : &str, distances: &Distances) -> u32 {
+    let mut distance_value = 0u32;
+    for distance in distances {
+        if (distance.point_a == place_a && distance.point_b == place_b) || 
+        (distance.point_a == place_b && distance.point_b == place_a) {
+            distance_value = distance.value;
+            break;
+        }
+    }
+
+    distance_value
 }
