b53d3dd44d1eaade0897fdd6037629f2d0dad60e
Ryszard <rustedwolf@gmail.com>
2016-05-15 03:54:07 +0200

    Day 8

---
+++ day_08/src/main.rs
@@ -0,0 +1,43 @@
+extern crate unescape;
+
+use std::path::Path;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use unescape::unescape;
+
+fn count_escaped(input: &str) -> usize {
+    let mut char_count = 2;
+    for symbol in input.chars() {
+        char_count += symbol.escape_default().count();
+    }
+    char_count
+}
+
+fn main() {
+    println!("--- Day 8: Matchsticks ---"); 
+
+    let path = Path::new("input/day_8.txt");
+    let input = match File::open(&path) {
+        Ok(file) => file,
+        Err(reason) => panic!("Could not open input file: {}", reason)
+    };
+
+    let reader = BufReader::new(&input);
+
+    let mut unescaped_char_sum = 0;
+    let mut escaped_char_sum = 0;
+
+    for line in reader.lines() {
+        let line_text = line.unwrap();
+        let chars: Vec<char> = line_text.chars().collect();
+
+        let enescaped_chars: Vec<char> = unescape(&line_text).unwrap().chars().collect();
+
+        unescaped_char_sum += chars.len() - (enescaped_chars.len() - 2);
+        escaped_char_sum += count_escaped(&line_text) - chars.len();
+    }
+
+    println!("Then unescaped char difference of chars is: {}", unescaped_char_sum); //musi być 1371
+    println!("Then escaped char difference of chars is: {}", escaped_char_sum);
+}
