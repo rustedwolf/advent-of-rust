a1ef53555ac45d8ecfcda5399c53a7ca9b51d233
Ryszard Narkiewicz <rustedwolf@gmail.com>
2016-09-18 04:13:32 +0200

    Day 19 part 1

---
+++ day_19/src/main.rs
@@ -0,0 +1,119 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+type Molecule = String;
+
+struct Replacement {
+    molecule: Molecule,
+    replacement: Molecule,
+}
+
+type Replacements = Vec<Replacement>;
+
+fn main() {
+    println!("--- Day 19: Medicine for Rudolph ---");
+
+    let (replacements, molecule) = get_data();
+    let mut molecule_list: Vec<Molecule> = generate_molecule_list(&replacements, &molecule);
+    molecule_list.sort();
+    molecule_list.dedup();
+
+    println!("There are {} of distinct molecules can be created ", molecule_list.len());
+}
+
+fn get_data() -> (Replacements, Molecule) {
+    let mut replacements: Replacements = Vec::new();
+    let mut molecule = "".to_string();
+
+    let input = get_input();
+    let reader = BufReader::new(&input);
+
+    for line in reader.lines() {
+        let line_info = line.unwrap();
+
+        if line_info.contains(" => ") {
+            replacements.push(parse_replacement(&line_info));
+        } else if line_info.len() > 0 {
+            molecule = line_info;
+        }
+    }
+
+    (replacements, molecule)
+}
+
+fn get_input() -> File {
+    let path = Path::new("data/input");
+    let input = match File::open(&path) {
+        Ok(file) => file,
+        Err(reason) => panic!("Could not open input file: {}", reason),
+    };
+
+    input
+}
+
+fn parse_replacement(info: &str) -> Replacement {
+    let replacement_info: Vec<&str> = info.split(" => ").collect();
+    let replacement = Replacement {
+        molecule: replacement_info[0].to_string(),
+        replacement: replacement_info[1].to_string()
+    };
+
+    replacement
+}
+
+fn generate_molecule_list(replacements: &Replacements, molecule: &str) -> Vec<Molecule> {
+    let mut molecule_list: Vec<Molecule> = Vec::new();
+
+    for replacement in replacements {
+        let mut replacement_molecules = generate_replacement_molecules(&replacement, molecule);
+        molecule_list.append(&mut replacement_molecules);
+    }
+
+    molecule_list
+}
+
+fn generate_replacement_molecules(replacement: &Replacement, molecule: &str) -> Vec<Molecule> {
+    let mut molecule_list: Vec<Molecule> = Vec::new();
+    let target_molecules: Vec<_> = molecule.match_indices(&replacement.molecule).collect();
+
+    for target_molecule in target_molecules {
+        let new_molecule = replace_molecule(replacement, &target_molecule, molecule);        
+        molecule_list.push(new_molecule);
+    }
+
+    molecule_list
+}
+
+fn replace_molecule(replacement: &Replacement, target_molecule: &(usize, &str), molecule: &str) -> Molecule {
+    let mut molecule_clone = molecule.to_string();
+    let (first, second) = molecule_clone.split_at_mut(target_molecule.0);
+    let (_, rest) = second.split_at(target_molecule.1.len());
+    let new_molecule = first.to_string() + &replacement.replacement + &rest;
+
+    new_molecule
+}
+
+#[test]
+fn test_replacement() {
+    let molecule = "HOH".to_string();
+    let replacements = vec![
+        Replacement { molecule: "H".to_string(), replacement: "HO".to_string()},
+        Replacement { molecule: "H".to_string(), replacement: "OH".to_string()},
+        Replacement { molecule: "O".to_string(), replacement: "HH".to_string()}
+    ];
+
+    let mut molecule_list = generate_molecule_list(&replacements, &molecule);
+
+    assert_eq!("HOOH", &molecule_list[0]);
+    assert_eq!("HOHO", &molecule_list[1]);
+    assert_eq!("OHOH", &molecule_list[2]);
+    assert_eq!("HOOH", &molecule_list[3]);
+    assert_eq!("HHHH", &molecule_list[4]);
+
+    molecule_list.sort();
+    molecule_list.dedup();
+
+    assert_eq!(4, molecule_list.len());
+}
