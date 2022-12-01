3bcb9500ef805b2a2d460c48217e169f3fa25b02
Ryszard Narkiewicz <rustedwolf@gmail.com>
2022-12-01 23:40:39 +0200

    2022, Day 1: Calorie Counting

---
+++ 2022/day_01/src/main.rs
@@ -0,0 +1,31 @@
+use std::fs::read_to_string;
+
+fn main() {
+    println!("--- Day 1: Calorie Counting ---");
+
+    let content = read_to_string("data/input").unwrap();
+    let calories_list: Vec<i32> = process_input(&content);
+    println!("The biggest amount of calories is: {}", calories_list[0]);
+
+    let top_3_total: i32 = calories_list[..3].iter().sum();
+    println!("The calories are three top Elves carrying is {}", top_3_total);
+}
+
+
+fn process_input(input: &str) -> Vec<i32> {
+    let mut calories = 0;
+    let mut calories_list: Vec<i32> = vec![];
+
+    for line in input.lines() {
+        if line != "" {
+            calories += line.parse::<i32>().unwrap();
+        } else {
+            calories_list.push(calories);
+            calories = 0;
+        }
+    }
+
+    calories_list.sort();
+
+    calories_list.iter().rev().map(|&x| x).collect()
+}
